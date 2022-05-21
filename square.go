1785e415f104a62fc2c1234670b04fa17e8bf799
Mikhail <mishajudoist@gmail.com>
2022-05-21 17:07:39 +0300

    implemented all methods v.1

--- square.go
+++ square.go
@@ -9,14 +9,20 @@ type Square struct {
 	a     uint
 }
 
-func (receiver) End() Point {
+func (s Square) End() Point {
 	// implement me
+	var end Point
+	end.x = s.start.x + int(s.a)
+	end.y = s.start.y - int(s.a)
+	return end
 }
 
-func (receiver) Area() uint {
+func (s Square) Area() uint {
 	// implement me
+	return s.a * s.a
 }
 
-func (receiver) Perimeter() uint {
+func (s Square) Perimeter() uint {
 	// implement me
+	return s.a * 4
 }
